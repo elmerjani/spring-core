1241a33fedcbbf072a9f19018b056652051b9635
elmerjani <mohamedelmerjani3@gmail.com>
2024-12-11 18:04:09 +0100

    Add Spring Boot Actuator to monitor the application

--- lab/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
+++ lab/44-actuator/src/main/java/accounts/web/RestaurantHealthCheck.java
@@ -1,5 +1,12 @@
 package accounts.web;
 
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+import rewards.internal.restaurant.Restaurant;
+import rewards.internal.restaurant.RestaurantRepository;
+
 /**
  * TODO-16a: Create custom health indicator
  * - Make this class implement HealthIndicator interface
@@ -9,8 +16,21 @@ package accounts.web;
  *   (no restaurants) or UP otherwise. (Note that RestaurantRepository
  *   has a method that returns number of restaurants.)
  */
-public class RestaurantHealthCheck {
+@Component
+public class RestaurantHealthCheck implements HealthIndicator {
 
+    RestaurantRepository  restaurantRepository;
+
+    public RestaurantHealthCheck(RestaurantRepository restaurantRepository) {
+        this.restaurantRepository = restaurantRepository;
+    }
+
+    @Override
+    public Health health() {
+
+        Long restaurantCount = restaurantRepository.getRestaurantCount();
+        return restaurantCount > 0 ? Health.up().build() : Health.status("NO_RESTAURANTS").build();
+    }
 }
 
 
